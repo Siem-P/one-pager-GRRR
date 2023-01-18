09484e0bfede9dda7d8d7d18a4d036a0e64585f7
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-01-18 23:30:26 +0100

    Added GSAP Animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,5 +1,31 @@
-import { gsap } from "gsap";
+gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".box", {
-//     x: 100,
-// });
+
+// Company name - Landing Page
+gsap.to(".company-name", {
+    top: 56,
+    ease: Power3.easeOut,
+    
+    duration: 2
+});
+
+// H1
+// gsap.to(".hero-intro h1", {
+
+// })
+
+// Five-Zero's
+gsap.to(".advertisement-section > span", {
+    scrollTrigger: {
+        trigger: ".advertisement-section",
+        start: "top 70%",
+        // end: "bottom 90%",
+        // markers: true,
+        // scrub: true,
+        toggleActions: "play none none reset"
+    },
+    stagger: 0.3,
+    x: 0,
+    duration: 1,
+    ease: Circ.easeOut
+});
